476b82307780112d5d5deff0701cc64dda6df7fb
limrp <limrod.15@gmail.com>
2023-10-29 16:41:05 -0500

    Creating post endpoint.

--- storeapi/main.py
+++ storeapi/main.py
@@ -1,8 +1,34 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello, world!"}
+# Models to validate incoming data
+class UserPostIn(BaseModel):
+    body: str
+
+
+class UserPost(UserPostIn):
+    id: int
+
+
+# Database
+post_table = {}
+
+
+# Creating post endpoint
+@app.post("/post", response_model=UserPost)
+async def create_post(post: UserPostIn):
+    data = post.model_dump()
+    last_record_id = len(post_table)
+    new_post = {**data, "id": last_record_id}
+    post_table[last_record_id] = new_post
+    return new_post
+    # Needs to respond with the body and id = new_post
+
+
+# To get a list of all the available
+
+# to-do
+# print post_table in jn
